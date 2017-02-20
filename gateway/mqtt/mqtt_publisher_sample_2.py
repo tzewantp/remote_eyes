106b31e43a014ece7b435b3c846dde014c46ab7b
Foo Tze Wan <tzewantp@gmail.com>
2017-02-20 17:52:31 +0800

    Create mqtt_publisher_sample_2.py

---
+++ gateway/mqtt/mqtt_publisher_sample_2.py
@@ -0,0 +1,35 @@
+import time
+import paho.mqtt.publish as publish
+import psutil
+
+# ThingSpeak Channel Settings
+channelID = "224072"
+apiKey = "IV4RT30CC3SUUT6L"
+mqttHost = "mqtt.thingspeak.com"
+
+# Connection Parameters
+tTransport = "tcp"
+tPort = 1883
+tTLS = None
+
+# Create topic string
+topic = "channels/" + channelID + "/publish/" + apiKey
+
+# Initial value
+tempValue = 32.1
+
+while True:
+
+   # Get the system performance data
+   cpuPercent = psutil.cpu_percent(interval=20)
+   ramPercent = psutil.virtual_memory().percent
+
+   tPayload = "field1=" + str(cpuPercent) + "&field2=" + str(ramPercent)
+   print("CPU = ", cpuPercent, " RAM = ", ramPercent)
+
+   try:
+      publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
+
+   except:
+      print("Error publishing")
+   
