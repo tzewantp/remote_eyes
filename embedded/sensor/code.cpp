7d84d18f46f78cfda7b58741fa1f258dc8cb2de5
Foo Tze Wan <tzewantp@gmail.com>
2017-06-08 21:38:41 +0800

    Create code.cpp

--- embedded/sensor/code.cpp
+++ embedded/sensor/code.cpp
@@ -1,4 +1,27 @@
-void main(void)
-{
+#include "mbed.h"
+ 
+Serial pcPort(SERIAL_TX, SERIAL_RX);  // Serial port for debugging
+Serial xbeePort(PB_6, PB_7);          // Serial port for XBee
+ 
+// send the alarm on/off message
+const unsigned char TestMsg[] = { 'T', 'e', 's', 't', '\r' };
 
+void xbee_Send(unsigned char *writeBuffer, int writeLen)
+{
+    int len;
+    
+    for (len = 0; len < writeLen; len++)
+        xbeePort.putc(writeBuffer[len]);
 }
+
+int main()
+{
+    pc.printf("XBee simulator program running....\n");
+    
+    while(1)
+    {
+        xbee_Send();
+        wait(5);
+    }
+}
+
